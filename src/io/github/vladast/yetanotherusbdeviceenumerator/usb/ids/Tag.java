5c4ef5fcb0c789da237b8cc093271176e6cef116
Vladimir Stankovic <vladast.dev@gmail.com>
2014-05-28 23:39:52 +0200

    Added new usb.ids package for ID-related properties

---
+++ src/io/github/vladast/yetanotherusbdeviceenumerator/usb/ids/Tag.java
@@ -0,0 +1,16 @@
+package io.github.vladast.yetanotherusbdeviceenumerator.usb.ids;
+
+public enum Tag {
+	ID,		// Identifier (VID/PID)
+	C,		// Class
+	AT,		// Audio class terminal types
+	HID,	// HID descriptor types
+	R,
+	BIAS,	// Physical descriptor bias types
+	PHY,	// Physical descriptor item types
+	HUT,	// HID usages
+	L,		// Language
+	HCC,	// HID descriptor country codes
+	VT,		// Video class terminal types
+	NO_TAG	// 
+}
---
+++ src/io/github/vladast/yetanotherusbdeviceenumerator/usb/ids/UsbProperty.java
@@ -0,0 +1,5 @@
+package io.github.vladast.yetanotherusbdeviceenumerator.usb.ids;
+
+public class UsbProperty {
+
+}
