089441be4063281e34ba52d65d71a7163bcd2298
Vladimir Stankovic <vladast.dev@gmail.com>
2014-05-29 21:47:23 +0200

    Added initial version of the class representing data read from device, such as VIDs and PIDs

---
+++ src/io/github/vladast/yetanotherusbdeviceenumerator/usb/UsbDeviceData.java
@@ -0,0 +1,58 @@
+package io.github.vladast.yetanotherusbdeviceenumerator.usb;
+
+public class UsbDeviceData {
+	private short vendorId;
+	private short productId;
+	private byte usbClass;
+	private byte usbProtocol;
+	/**
+	 * @return the vendorId
+	 */
+	public short getVendorId() {
+		return vendorId;
+	}
+	/**
+	 * @param vendorId the vendorId to set
+	 */
+	public void setVendorId(short vendorId) {
+		this.vendorId = vendorId;
+	}
+	/**
+	 * @return the productId
+	 */
+	public short getProductId() {
+		return productId;
+	}
+	/**
+	 * @param productId the productId to set
+	 */
+	public void setProductId(short productId) {
+		this.productId = productId;
+	}
+	/**
+	 * @return the usbClass
+	 */
+	public byte getUsbClass() {
+		return usbClass;
+	}
+	/**
+	 * @param usbClass the usbClass to set
+	 */
+	public void setUsbClass(byte usbClass) {
+		this.usbClass = usbClass;
+	}
+	/**
+	 * @return the usbProtocol
+	 */
+	public byte getUsbProtocol() {
+		return usbProtocol;
+	}
+	/**
+	 * @param usbProtocol the usbProtocol to set
+	 */
+	public void setUsbProtocol(byte usbProtocol) {
+		this.usbProtocol = usbProtocol;
+	}
+	
+	
+}
