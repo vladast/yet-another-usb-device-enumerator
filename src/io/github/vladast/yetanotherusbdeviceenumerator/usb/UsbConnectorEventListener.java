1dd3a89b3a45980033ac3c484e227e900092e741
Vladimir Stankovic <vladast.dev@gmail.com>
2014-05-29 21:46:41 +0200

    Added initial event signatures

--- src/io/github/vladast/yetanotherusbdeviceenumerator/usb/UsbConnectorEventListener.java
+++ src/io/github/vladast/yetanotherusbdeviceenumerator/usb/UsbConnectorEventListener.java
@@ -1,5 +1,8 @@
 package io.github.vladast.yetanotherusbdeviceenumerator.usb;
 
 public interface UsbConnectorEventListener {
-
+	public void OnPollDevices();
+	public void OnDeviceDataRead(UsbDeviceData usbDeviceData);
+	public void OnErrorMessage(String message);
+	public void OnDebugMessage(String message);
 }
